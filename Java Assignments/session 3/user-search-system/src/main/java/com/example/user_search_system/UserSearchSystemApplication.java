99176e75859630d6f6da7e23510b082976a575f6
mahakdhanotiya <dhanotiyamahak14@gmail.com>
2026-04-13 23:44:50 +0530

    Initial project setup using Spring Initializr

---
+++ Java Assignments/session 3/user-search-system/src/main/java/com/example/user_search_system/UserSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_search_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSearchSystemApplication.class, args);
+	}
+
+}
---
+++ Java Assignments/session 3/user-search-system/src/test/java/com/example/user_search_system/UserSearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_search_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSearchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
