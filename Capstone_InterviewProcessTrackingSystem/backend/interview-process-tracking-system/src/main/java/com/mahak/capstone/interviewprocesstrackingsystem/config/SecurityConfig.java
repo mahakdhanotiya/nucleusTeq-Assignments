613f7d99894eb96782b95681fd3e77fa77e49f7e
mahakdhanotiya <dhanotiyamahak14@gmail.com>
2026-04-21 22:07:02 +0530

    fix(auth): update security config

--- Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/config/SecurityConfig.java
+++ Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/config/SecurityConfig.java
@@ -2,11 +2,27 @@ package com.mahak.capstone.interviewprocesstrackingsystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable()) // disable csrf for testing
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll() 
+            );
+
+        return http.build();
+    }
+
+
 
     //  password encoder bean
     @Bean
