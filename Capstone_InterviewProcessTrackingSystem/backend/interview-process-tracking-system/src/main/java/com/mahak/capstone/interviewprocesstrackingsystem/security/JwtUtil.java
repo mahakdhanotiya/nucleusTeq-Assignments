2c4a8672ec19003ea4ef5fa15cd1cce4cf784ab5
mahakdhanotiya <dhanotiyamahak14@gmail.com>
2026-04-21 21:57:13 +0530

    feat(auth): add JWT utility and externalized configuration

---
+++ Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/security/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.mahak.capstone.interviewprocesstrackingsystem.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    // generate token
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+    // extract email
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
