f5123c6ddd6ba38ab8faf12554480a10b835f59b
mahakdhanotiya <dhanotiyamahak14@gmail.com>
2026-04-22 00:33:21 +0530

    feat(auth): enable role-based authentication with JWT filter and method security

--- Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/config/SecurityConfig.java
+++ Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.mahak.capstone.interviewprocesstrackingsystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.mahak.capstone.interviewprocesstrackingsystem.security.JwtAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
--- Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/security/JwtAuthenticationFilter.java
+++ Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/security/JwtAuthenticationFilter.java
@@ -1,11 +1,18 @@
 package com.mahak.capstone.interviewprocesstrackingsystem.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.mahak.capstone.interviewprocesstrackingsystem.entity.User;
+import com.mahak.capstone.interviewprocesstrackingsystem.repository.UserRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,8 +27,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // This utility will help us extract data from JWT
     private final JwtUtil jwtUtil;
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -44,15 +54,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 String email = jwtUtil.extractEmail(token);
 
                 // If email exists → token is valid
-                if (email != null) {
-                    logger.info("Valid token for user: {}", email);
+                if (email != null && SecurityContextHolder.getContext().getAuthentication() == null)  {
+
+                    // Fetch user from DB
+                    User user = userRepository.findByEmail(email)
+                            .orElseThrow(() -> new RuntimeException("User not found"));
+
+                    // Get role from DB
+                    String role = user.getRole().name();
+
+                    // Create authentication object with role
+                    UsernamePasswordAuthenticationToken auth =
+                            new UsernamePasswordAuthenticationToken(
+                                    email,
+                                    null,
+                                    List.of(new SimpleGrantedAuthority("ROLE_" + role))
+                            );
+
+                    // Set authentication in Spring Security context
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+
+                    logger.info("User authenticated: {} with role: {}", email, role);
                 }
 
-            } catch (Exception e) {
-                // If token is invalid
-                logger.error("Invalid token");
-            }
+         } catch (Exception e) {
+            // If token is invalid
+            logger.error("Invalid token: {}", e.getMessage());
         }
+     }
 
         // Continue request flow 
         filterChain.doFilter(request, response);
