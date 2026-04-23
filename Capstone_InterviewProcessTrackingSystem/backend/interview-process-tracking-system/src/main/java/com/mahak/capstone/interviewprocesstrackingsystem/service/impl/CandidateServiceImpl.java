88c8047fc81069ad9087c05651bd7f99d0116f88
mahakdhanotiya <dhanotiyamahak14@gmail.com>
2026-04-23 23:55:39 +0530

    refactor(candidate): improve service layer logic and validation with business rules

--- Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/service/impl/CandidateServiceImpl.java
+++ Capstone_InterviewProcessTrackingSystem/backend/interview-process-tracking-system/src/main/java/com/mahak/capstone/interviewprocesstrackingsystem/service/impl/CandidateServiceImpl.java
@@ -1,16 +1,25 @@
 package com.mahak.capstone.interviewprocesstrackingsystem.service.impl;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.mahak.capstone.interviewprocesstrackingsystem.constants.ErrorConstants;
 import com.mahak.capstone.interviewprocesstrackingsystem.dto.CandidateRequestDTO;
 import com.mahak.capstone.interviewprocesstrackingsystem.dto.CandidateResponseDTO;
 import com.mahak.capstone.interviewprocesstrackingsystem.entity.CandidateProfile;
 import com.mahak.capstone.interviewprocesstrackingsystem.entity.JobDescription;
 import com.mahak.capstone.interviewprocesstrackingsystem.entity.User;
+import com.mahak.capstone.interviewprocesstrackingsystem.enums.ApplicationStatus;
+import com.mahak.capstone.interviewprocesstrackingsystem.exception.InvalidRequestException;
+import com.mahak.capstone.interviewprocesstrackingsystem.exception.ResourceNotFoundException;
 import com.mahak.capstone.interviewprocesstrackingsystem.mapper.CandidateMapper;
 import com.mahak.capstone.interviewprocesstrackingsystem.repository.CandidateRepository;
 import com.mahak.capstone.interviewprocesstrackingsystem.repository.JobDescriptionRepository;
 import com.mahak.capstone.interviewprocesstrackingsystem.repository.UserRepository;
+import com.mahak.capstone.interviewprocesstrackingsystem.security.CurrentUserUtil;
 import com.mahak.capstone.interviewprocesstrackingsystem.service.CandidateService;
 import com.mahak.capstone.interviewprocesstrackingsystem.validation.CandidateValidator;
 
@@ -22,13 +31,13 @@ import com.mahak.capstone.interviewprocesstrackingsystem.validation.CandidateVal
 @Service
 public class CandidateServiceImpl implements CandidateService {
 
-    // Repository to interact with database
+    private static final Logger logger = LoggerFactory.getLogger(CandidateServiceImpl.class);
+
     private final CandidateRepository candidateRepository;
     private final UserRepository userRepository;
     private final JobDescriptionRepository jobRepository;
 
-    
-    // constructor injection
+    // Constructor Injection
     public CandidateServiceImpl(
             CandidateRepository candidateRepository,
             UserRepository userRepository,
@@ -38,35 +47,116 @@ public class CandidateServiceImpl implements CandidateService {
         this.userRepository = userRepository;
         this.jobRepository = jobRepository;
     }
-    
+
     /**
-     * Creates and saves a new candidate profile in the database
-     *
-     * @param candidate CandidateProfile object containing candidate details
-     * @return saved CandidateProfile
+     * Create new candidate application
      */
-    
     @Override
     public CandidateResponseDTO createCandidate(CandidateRequestDTO requestDTO) {
 
-        // validate request before processing
+        logger.info("Creating candidate for userId: {} and jobId: {}",
+                requestDTO.getUserId(), requestDTO.getJobId());
+
+        // Validate request
         CandidateValidator.validateCreateCandidate(requestDTO);
 
-        // get user from DB
+        // Fetch User
         User user = userRepository.findById(requestDTO.getUserId())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> {
+                    logger.error("User not found with id: {}", requestDTO.getUserId());
+                    return new ResourceNotFoundException(ErrorConstants.USER_NOT_FOUND);
+                });
 
-        // get job from DB
+        //Check active application 
+        boolean exists = candidateRepository
+                .existsByUserAndApplicationStatusNot(user, ApplicationStatus.REJECTED);
+
+        if (exists) {
+            logger.error("Active application already exists for userId: {}", requestDTO.getUserId());
+            throw new InvalidRequestException(ErrorConstants.ACTIVE_APPLICATION_EXISTS);
+        }
+
+        // Fetch Job
         JobDescription job = jobRepository.findById(requestDTO.getJobId())
-                .orElseThrow(() -> new RuntimeException("Job not found"));
+                .orElseThrow(() -> {
+                    logger.error("Job not found with id: {}", requestDTO.getJobId());
+                    return new ResourceNotFoundException(ErrorConstants.JOB_NOT_FOUND);
+                });
 
-        // convert DTO → Entity
-        CandidateProfile candidate = CandidateMapper.toEntity(requestDTO, user, job);
+        // Validate Resume (PDF only)
+        if (!requestDTO.getResumeUrl().toLowerCase().endsWith(".pdf")) {
+            logger.error("Invalid resume format for userId: {}", requestDTO.getUserId());
+            throw new InvalidRequestException(ErrorConstants.INVALID_RESUME_FORMAT);
+        }
 
-        // save in DB
+        // Map DTO → Entity
+        CandidateProfile candidate =
+                CandidateMapper.toEntity(requestDTO, user, job);
+
+        // Save in DB
         CandidateProfile saved = candidateRepository.save(candidate);
 
-        // convert Entity → ResponseDTO
+        logger.info("Candidate created successfully with id: {}", saved.getId());
+
+        // Return Response DTO
         return CandidateMapper.toDTO(saved);
+    }
+
+    /**
+     * Fetch all candidates
+     */
+    @Override
+    public List<CandidateResponseDTO> getAllCandidates() {
+
+        logger.info("Fetching all candidates");
+
+        List<CandidateResponseDTO> list = candidateRepository.findAll()
+                .stream()
+                .map(CandidateMapper::toDTO)
+                .toList();
+
+        logger.info("Total candidates fetched: {}", list.size());
+
+        return list;
+    }
+
+    /**
+     * Fetch candidate by ID
+     */
+    @Override
+    public CandidateResponseDTO getCandidateById(Long id) {
+
+        logger.info("Fetching candidate with id: {}", id);
+
+        CandidateProfile candidate = candidateRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.error("Candidate not found with id: {}", id);
+                    return new ResourceNotFoundException(ErrorConstants.CANDIDATE_NOT_FOUND);
+                });
+        return CandidateMapper.toDTO(candidate);
+    }
+
+    @Override
+    public CandidateResponseDTO getMyProfile() {
+
+        logger.info("Fetching logged-in candidate profile");
+
+        String email = CurrentUserUtil.getCurrentUserEmail();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    logger.error("User not found with email: {}", email);
+                    return new ResourceNotFoundException(ErrorConstants.USER_NOT_FOUND);
+                });
+
+        CandidateProfile candidate = candidateRepository.findByUser(user)
+                .orElseThrow(() -> {
+                    logger.error("Candidate not found for user: {}", user.getId());
+                    return new ResourceNotFoundException(ErrorConstants.CANDIDATE_NOT_FOUND);
+                });
+
+        logger.info("Profile fetched successfully for user: {}", user.getId());
+
+        return CandidateMapper.toDTO(candidate);
     }
 }
